324385a8eb668488a06ad37b5cc3b1743bc4b1c4
Robelo Steven <20299@columbushs.com>
2019-11-21 09:09:41 -0500

    commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,50 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(array){
+    let copy = [...array];
+
+    copy.sort((a,b) => {
+        if (a.year < b.year){
+            return -1;
+        }
+        else if (b.year < a.year){
+            return 1;
+        }
+        else{
+            if(a.title < b.title){
+                return -1;
+            }
+            else if(b.title < a.title){
+                return 1
+            }
+            else{
+                return 0;
+            }
+        }
+    })
+
+    return copy;
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(movies){
+    
+    let moviesBySteven = movies.filter((eachMovie) => {
+        return eachMovie.director == "Steven Spielberg" && eachMovie.genre.includes("Drama");
+
+    })
+
+    return moviesBySteven.length;
+}
+
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(movies){
+    
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
