f06b1ae8afb1b29143984bffd1d9e887a68b2455
Steven Robelo <srobelo20299@columbushs.com>
2019-11-24 20:07:06 -0500

    commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -43,14 +43,65 @@ function howManyMovies(movies){
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
-function orderAlphabetically(movies){
-    
-}
+function orderAlphabetically(titleorder) {
+    let copy = [...titleorder]
+  
+
+    copy.sort((a, b) => {
+      if (a.title < b.title) {
+        return -1;
+      } else if (b.title < a.title) {
+        return 1;
+      } else {
+        return 0
+      }
+    })
+  
+    let titleArray = copy.map((eachMovie)=> {
+        return eachMovie.title 
+        })
+
+        return titleArray.splice(0,20)
+    }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(array){
+    if(array.length == 0){
+      return 0
+    }
+    let rates = array.map( (eachMovie)=>{
+      if(eachMovie.rate){
+      return eachMovie.rate
+      } else{
+        return 0
+      }
+    })
+    let sumRates = rates.reduce( (a, c)=>{
+      return a + c
+    })
+    let finalAverage = Number((sumRates / array.length).toFixed(2))
+    console.log(typeof(finalAverage))
+    return finalAverage
+  }
+
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(array){
+    let dramaMovies = array.filter( (eachMovie)=>{
+      return eachMovie.genre == "Drama";
+    })
+    return ratesAverage(dramaMovies)
+  };
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+function turnHoursToMinutes (duration) {
+    var minDuration = 0;
+    var hours = 0;
+    var mins = 0;
+    var geth = duration.indexOf("hours");
+    var getmin = duration.indexOf("mins");
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
